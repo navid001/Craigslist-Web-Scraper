2a111bcad4839f4c0b04c61304a5a5434345053b
navid001 <navidalvi.001@gmail.com>
2022-03-16 12:41:41 +0600

    Pushing Frontend Search interface

--- app/views.py
+++ app/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
 
 # Create your views here.
 def home(request):
     return render(request,'base.html')
 
 def new_search(request):
-    return render(request,'new_search.html')
+    url='https://newyork.craigslist.org/search/bbb?query='
+    search=request.POST.get('search')
+    final_url=url+search
+    print(final_url)
+    search_parameters = {
+        'search':search,
+    }
+    return render(request,'new_search.html',search_parameters)
